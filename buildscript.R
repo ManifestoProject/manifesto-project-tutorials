cb7a15b1654fff59291e81a91c74acb9c5462377
Nicolas Merz <nicolas.merz@wzb.eu>
2017-02-13 14:28:21 +0100

    typo

--- buildscript.R
+++ buildscript.R
@@ -4,7 +4,7 @@ install.packages("bookdown")
 
 library(manifestoR)
 library(bookdown)
-
+library(rmarkdown)
 
 mp_setapikey(key.file = "manifesto_apikey.txt")
-render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
+render_site(output_format = "bookdown::gitbook", encoding = "UTF-8")
